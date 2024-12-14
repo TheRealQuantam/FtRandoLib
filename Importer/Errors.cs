43390cb721619f7a3b1843a381ed91c05325bc21
TheRealQuantam <54358053+TheRealQuantam@users.noreply.github.com>
2024-12-13 16:13:40 -0800

    Validate uses at song load

---
+++ Importer/Errors.cs
@@ -0,0 +1,22 @@
+﻿using FtRandoLib.Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FtRandoLib.Importer;
+
+public class InvalidUsageError : Exception
+{
+    public string Usage { get; }
+    public ISong Song { get; }
+
+    public InvalidUsageError(string usage, ISong song)
+        : base($"invalid usage '{usage}' in song '{song.Title}'")
+    {
+        Usage = usage;
+        Song = song;
+    }
+}
+
+public class RomFullException : Exception { }
+
